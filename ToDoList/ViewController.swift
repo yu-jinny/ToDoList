c093b2f8014cc82ecd2278977da2f9763b9ba00e
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2023-12-21 02:15:04 +0900

    1

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -7,41 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var toDoListTableView: UITableView!
     @IBOutlet weak var addNewListButton: UIButton!
     
     var toDoMemo : [String] = ["할 일1", "할 일 2", "할 일 3"]
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         toDoListTableView.backgroundColor = .yellow
         toDoListTableView.delegate = self
         toDoListTableView.dataSource = self
-        toDoListTableView.setEditing(true, animated: true)
-           
+        addNewListButton.addTarget(self, action: #selector(addNewListButtonTapped), for: .touchUpInside)
     }
+    
     @IBAction func addNewListButtonTapped(_ sender: Any) {
-        toDoMemo.append("")
-             let indexPath = IndexPath(row: toDoMemo.count - 1, section: 0)
-                     toDoListTableView.insertRows(at: [indexPath], with: .automatic)
-        if let cell = toDoListTableView.cellForRow(at: indexPath) as? MyFirstCell{
-            cell.textView.becomeFirstResponder()
-        }
+        showAddTodoMessage()
     }
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func showAddTodoMessage() {
+        let alert = UIAlertController(title: "할 일 추가", message: nil, preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "할 일을 입력하세요"
+        }
+        
+        let addAction = UIAlertAction(title: "추가", style: .default) { [weak self] _ in
+            if let textField = alert.textFields?.first, let newTodo = textField.text, !newTodo.isEmpty {
+                self?.addTodo(newTodo)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func addTodo(_ todo: String) {
+        toDoMemo.append(todo)
+        toDoListTableView.reloadData()
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDoMemo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = toDoListTableView.dequeueReusableCell(withIdentifier: "MyFirstCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
         cell.textLabel?.text = toDoMemo[indexPath.row]
         return cell
     }
-}
 
+}
