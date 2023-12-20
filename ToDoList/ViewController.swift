274632e1dccbd7fbffb75933ba3b65ae0bc05fcd
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2023-12-21 01:51:03 +0900

    아...

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var toDoListTableView: UITableView!
+    @IBOutlet weak var addNewListButton: UIButton!
+    
+    var toDoMemo : [String] = ["할 일1", "할 일 2", "할 일 3"]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        toDoListTableView.backgroundColor = .yellow
+        toDoListTableView.delegate = self
+        toDoListTableView.dataSource = self
+        toDoListTableView.setEditing(true, animated: true)
+           
     }
+    @IBAction func addNewListButtonTapped(_ sender: Any) {
+        toDoMemo.append("")
+             let indexPath = IndexPath(row: toDoMemo.count - 1, section: 0)
+                     toDoListTableView.insertRows(at: [indexPath], with: .automatic)
+        if let cell = toDoListTableView.cellForRow(at: indexPath) as? MyFirstCell{
+            cell.textView.becomeFirstResponder()
+        }
+    }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoMemo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = toDoListTableView.dequeueReusableCell(withIdentifier: "MyFirstCell", for: indexPath)
+        cell.textLabel?.text = toDoMemo[indexPath.row]
+        return cell
+    }
 }
 
